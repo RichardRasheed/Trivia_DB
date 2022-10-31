33051d0faa00f35d7345933f72c81b470d2602ba
Richard Rasheed <richardrobertrasheed@gmail.com>
2022-10-31 00:09:43 -0400

    Finished homework

--- src/server.js
+++ src/server.js
@@ -7,15 +7,15 @@ const cookieParser = require('cookie-parser');
 // create a new server application
 const app = express();
 
+//adding node fetch to our implementation
+const fetch = require('node-fetch');
+
 // Define the port we will listen on
 // (it will attempt to read an environment global
 // first, that is for when this is used on the real
 // world wide web).
 const port = process.env.PORT || 3000;
 
-
-//adding node fetch to our implementation
-const fetch = require('node-fetch');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -49,8 +49,6 @@ app.get('/', (req, res) => {
   console.log(req.cookies);
 });
 
-
-//adding more to the trivia endpoint
 app.get("/trivia", async (req, res) => {
   // fetch the data
   const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
@@ -63,18 +61,39 @@ app.get("/trivia", async (req, res) => {
   }
 
   // interpret the body as json
-  const content = await response.json();
+  const data = await response.json();
 
   // fail if db failed
-  if (response.response_code !== 0) {
+  if (data.response_code !== 0) {
     res.status(500);
-    res.send(`Open Trivia Database failed with internal response code ${response.response_code}`);
+    res.send(`Open Trivia Database failed with internal response code ${data.response_code}`);
     return;
   }
 
   // respond to the browser
   // TODO: make proper html
-  res.render('trivia');
+
+
+
+  //creating real HTML now
+  const correctAnswer = data.results[0].correct_answer;
+  const answers = [data.results[0].incorrect_answers,correctAnswer];
+  const answerLinks = answers.map(answer => {
+    return `<a href="javascript:alert('${
+      answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+      }')">${answer}</a>
+    `
+  })
+
+
+  res.render('trivia',{
+    question: data.results[0].question,
+    answerLinks,
+    correct_answer: data.results[0].correct_answer,
+    answers: data.results[0].incorrect_answers,
+    category: data.results[0].category,
+    difficulty: data.results[0].difficulty
+  })
 });
 
 // Start listening for network connections
